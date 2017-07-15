6558e55550d30778261badbc20f9190b92b989fd
KEOTL <kento.otomo-lauzon.1@ulaval.ca>
2017-07-15 13:06:03 -0400

    Added gradlew

--- mockBackend/routes.py
+++ mockBackend/routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, session, redirect
-import urllib.request
+from flask import Flask, request, session, redirect;
+import urllib.request;
+import json;
 
 app = Flask(__name__);
 
@@ -8,7 +9,8 @@ def smsReceived():
     id = request.form.get('id');
 
     print(id);
-    print(urllib.request.urlopen("http://sms-line1.internal.lambdanum.com/get_message.py?message_id={}".format(id)).read());
+    message = json.loads(urllib.request.urlopen("http://sms-line1.internal.lambdanum.com/get_message.py?message_id={}".format(id)).read());
+    print (urllib.request.urlopen("http://sms-line1.internal.lambdanum.com/send_message.py?destination={}&message={}".format(message['chat_identifier'].strip('+'),"Thank%20you,%20your%20message%20has%20been%20received.%20%23{}.".format(message['id']))).read());
     return 'OK';
     
 if __name__ == "__main__":
